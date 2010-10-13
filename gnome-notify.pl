86c72d664439603ea7e083cd40d67ee5e12141f1
Erwin <erwinatuli@gmail.com>
2010-10-13 18:37:42 +0800

    removed debug statements and removed the hardcoded nick

--- gnome-notify.pl
+++ gnome-notify.pl
@@ -13,15 +13,13 @@ $VERSION = '1.00';
 
 sub send_gnome_notification {
     my($from, $msg) = @_;
-    print $from;
-    print $msg;
     system("notify-send \"$from\" \"$msg\"");
 }
 
 sub event_privmsg {
     my ($server, $data, $nick, $address) = @_;
-    #how do i get my own nick????
-    my $own_nick = 'slowlearner';
+    my $own_nick = $server->{nick};
+    
     if($data =~ /$own_nick/i && $data =~ /^(.*?):(.*)/) {
         my($source, $msg) = split(':', $data, 2);
         send_gnome_notification($source,  "[$nick] $msg");
